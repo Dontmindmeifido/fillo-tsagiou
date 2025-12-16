ffe8e6333a39a2daadaee5fe2cf5913e9c75a306
Ruslan <ruslangaitur@gmail.com>
2025-12-16 08:51:44 +0200

    touchups

--- attributes/js/login.js
+++ attributes/js/login.js
@@ -17,8 +17,41 @@ window.onload = () => {
         for (let i = 0; i < login_data.data.login.length; i++) {
             if (login_data.data.login[i] == login && login_data.data.password[i] == password) {
                 window.location.href = "http://127.0.0.1:5500/main.html"
-                break;
+                document.getElementById("response").style = "visibility: hidden; position: absolute;"
+                localStorage.setItem("loggedin", "true")
+                return
             }
+        }
+
+        document.getElementById("response").style = "visibility: visible; position: relative;"
+    })
+
+    document.addEventListener("keypress", (event) => {
+        if (event.key == "Enter") {
+            let login = document.getElementById("login").value
+            let password = document.getElementById("password").value
+
+            const xhr = new XMLHttpRequest()
+            xhr.open("GET", "http://127.0.0.1:5500/attributes/data/user_login_data.json", false) // false for no ascynchroneity
+            let jsonResponse = ""
+            xhr.onload = function() {
+                    // RAW AJAX returns a string. You must parse it manually.
+                    jsonResponse = xhr.responseText
+            }
+            xhr.send()
+
+            let login_data = JSON.parse(jsonResponse)
+
+            for (let i = 0; i < login_data.data.login.length; i++) {
+                if (login_data.data.login[i] == login && login_data.data.password[i] == password) {
+                    window.location.href = "http://127.0.0.1:5500/main.html"
+                    document.getElementById("response").style = "visibility: hidden; position: absolute;"
+                    localStorage.setItem("loggedin", "true")
+                    return
+                }
+            }
+
+            document.getElementById("response").style = "visibility: visible; position: relative;"
         }
     })
 }
--- attributes/js/logout.js
+++ attributes/js/logout.js
@@ -1,7 +1,12 @@
+if (localStorage.getItem("loggedin") == "true") {
+    // Great!
+} else {
+    window.location.href = "http://127.0.0.1:5500/index.html"
+}
+
 window.onload = () => {
     document.getElementById("logout").addEventListener("click", () => {
-        // Process
-
+        localStorage.setItem("loggedin", "false")
         window.location.href = "http://127.0.0.1:5500/index.html"
     })
 }
